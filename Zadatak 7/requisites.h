56857a6ca8ca5cd902004b53b40e2722bd7b89e6
stipe-jurkovic <115549802+stipe-jurkovic@users.noreply.github.com>
2022-12-15 19:22:58 +0100

    added the rest of task.

--- Zadatak 7/requisites.h
+++ Zadatak 7/requisites.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #define _CRT_SECURE_NO_WARNINGS
-#define MAX_CHAR_NUMBERS 50
-#define MAX_COMMAND_LENGTH 10
-#define MAX_DIRECTORY_LENGTH 40
+#define MAX_BUFFER_SIZE 1024
+#define MAX_COMMAND_LENGTH 1024
+#define MAX_DIRECTORY_LENGTH 1024
 #define ERROR 1
+
+#define PROGRAM_FAIL -1
+#define PROGRAM_SUCCESS 0
+#define EXIT 3
 
 typedef struct Directory* Position;
 
@@ -12,11 +16,28 @@ typedef struct Directory {
     char name[MAX_DIRECTORY_LENGTH];
     Position child;
     Position sibling;
-} Directory;
+} Directory; 
+typedef struct stog* Poz;
+
+typedef struct stog {
+    Position adr;
+    Poz next;
+    Poz prev;
+} Stog;
 
 // md, cd Directory, cd.., Directory, exit
 
-int readFromConsole(Position P);
-Position makeDirectory(Position P, Position Q);
+int initStog(Poz S);
+int initDir(Position q);
+int printMsg();
+int consoleHandler(Position *Padr, Poz stack);
+int makeDir(Position P, char name[MAX_DIRECTORY_LENGTH]);
+Position insertDirectory(Position P, Position Q);
+int changeDir(Position* Padr, Poz stack, char option[MAX_DIRECTORY_LENGTH]);
 int printContent(Position P);
-int changeDirectory(const char* directoryName);
+int printSiblings(Position Q);
+int printPath(Poz stack, Position P);
+int push(Poz prev, Position P);
+Position pop(Poz prev);
+int deleteTree(Position P);
+int emptyStack(Poz stack, Position* root);
