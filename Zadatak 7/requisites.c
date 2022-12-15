ae28c1a1804efe37ffe0855de120199a8c2d43e2
stipe-jurkovic <115549802+stipe-jurkovic@users.noreply.github.com>
2022-12-15 19:21:04 +0100

    added the rest of the task.

--- Zadatak 7/requisites.c
+++ Zadatak 7/requisites.c
@@ -1,65 +1,211 @@
 #include "requisites.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int readFromConsole(Position P)
+int initStog(Poz S) {
+	S->adr = NULL;
+	S->next = NULL;
+	S->prev = NULL;
+	return PROGRAM_SUCCESS;
+}
+int initDir(Position q) {
+
+	memset(q->name, 0, MAX_DIRECTORY_LENGTH);
+	q->child = NULL;
+	q->sibling = NULL;
+	return PROGRAM_SUCCESS;
+}
+int printMsg() {
+
+	printf(" \n");
+	printf(" LastDance Terminal\n");
+	printf(" Copyright(C) LastDance Corporation.All rights reserved.\n");
+	printf("\n Supported commands: md, cd <dir>, cd .., dir, exit.\n");
+	printf(" \n");
+	return PROGRAM_SUCCESS;
+}
+int consoleHandler(Position *Padr, Poz stack)
 {
+	char buffer[MAX_BUFFER_SIZE] = "\0";
 	char command[MAX_COMMAND_LENGTH] = "\0";
-	char directoryName[MAX_DIRECTORY_LENGTH] = "\0";
-	char buffer[MAX_CHAR_NUMBERS] = {0};
+	char option[MAX_DIRECTORY_LENGTH] = "\0";
+	char option2[MAX_DIRECTORY_LENGTH] = "\0";
+	Position P;
+	int n = 0;
+	P = *Padr;
 	
-	printf("%s/>", P->name);
+	memset(command, 0, MAX_COMMAND_LENGTH);
+	memset(option, 0, MAX_DIRECTORY_LENGTH);
+	memset(option2, 0, MAX_DIRECTORY_LENGTH);
+	memset(buffer, 0, MAX_BUFFER_SIZE);
+
+	printPath(stack, P);
+
 	gets(buffer);
-	sscanf(buffer, " %s %s", command, directoryName);
-	
+
+	n = sscanf(buffer, " %s %s %s", command, option, option2);
+	if (n > 2) {
+		printf("Too many arguments!\n");
+		return PROGRAM_FAIL;
+	}
+	else if (strlen(command)<2 && n == -1)
+		return PROGRAM_FAIL;
 	if (!strcmp(command, "dir"))
 	{
 		printContent(P);
+		return PROGRAM_SUCCESS;
 	}
-	
-	if (!strcmp(command, "exit"))
+	else if (!strcmp(command, "exit"))
 	{
-		return 0;
+		return EXIT;
 	}
-	
 	else if (!strcmp(command, "md") || !strcmp(command, "mkdir"))
 	{
-		Directory A;
-		Position Q = (Position)malloc(sizeof(Directory));
-		strcpy(A.name, directoryName);
-		A.child = NULL;
-		A.sibling = NULL;
-		Q = &A;
-		P->child = makeDirectory(P->child, Q);
+		return makeDir(P, option);
 	}
-	
-	return 0;
+	else if (!strcmp(command, "cd"))
+	{	
+		changeDir(Padr, stack, option);
+		return PROGRAM_SUCCESS;
+	}
+	printf("Unrecognized input!\n");
+	return PROGRAM_SUCCESS;
 }
-
-Position makeDirectory(Position P, Position Q)
+int makeDir(Position P, char name[MAX_DIRECTORY_LENGTH]) {
+	Position Q;
+	Q = malloc(sizeof(Directory));
+	if (Q == NULL)
+	{
+		printf("Insufficient ram space!");
+		return PROGRAM_FAIL;
+	}
+	initDir(Q);
+	strcpy(Q->name, name);
+	P->child = insertDirectory(P->child, Q);
+	return PROGRAM_SUCCESS;
+}
+Position insertDirectory(Position P, Position Q)
 {
 	if (P == NULL)
 		return Q;
-
+	if (strcmp(P->name, Q->name) == 0) {
+		printf("Directory exists!\n");
+		return P;
+	}
 	if (strcmp(P->name, Q->name) > 0)
 	{
 		Q->sibling = P;
 		return Q;
 	}
 
-	P->sibling = makeDirectory(P->sibling, Q);
+	P->sibling = insertDirectory(P->sibling, Q);
 	return P;
 }
-
+int changeDir(Position* Padr, Poz stack, char option[MAX_DIRECTORY_LENGTH])
+{
+	Position Ptemp,  P = *Padr;
+	Ptemp = P->child;
+	if (!strcmp(option, "..") && stack->next!=NULL) 
+	{
+		*Padr = pop(stack);
+	}
+	else
+	{
+		while (Ptemp != NULL) {
+		if (!strcmp(option, Ptemp->name))
+		{
+			push(stack, P);
+			*Padr  = Ptemp;
+			return PROGRAM_SUCCESS;
+		}
+		Ptemp = Ptemp->sibling;
+		}
+		if (Ptemp == NULL) {
+			printf("Directory not found!\n");
+		}
+	}
+	return PROGRAM_SUCCESS;
+}
 int printContent(Position P)
 {
-	Position Q = P->child;
-	//Q = P;
-	while (Q != NULL)
-	{
-		printf("\n %s\n", P->name);
-		Q = Q->sibling;
+	return printSiblings(P->child);
+}
+int printSiblings(Position Q) 
+{
+	if (Q == NULL) {
+		return PROGRAM_SUCCESS;
 	}
 
-	return 0;
+	printf("%s\n", Q->name);
+	printSiblings(Q->sibling);
+	//printf("%s\n", Q->name);
+
+	Q = Q->sibling;
+
+	return PROGRAM_SUCCESS;
+}
+int printPath(Poz stack, Position P) 
+{
+	Poz stacktemp = stack->next;
+	while (stacktemp != NULL && stacktemp->next != NULL) {
+		stacktemp = stacktemp->next;
+	}
+	while (stacktemp != NULL && stacktemp->prev != NULL)
+	{
+		printf("%s\\", stacktemp->adr->name);
+		stacktemp = stacktemp->prev;
+	}
+	printf("%s>", P->name);
+	return PROGRAM_SUCCESS;
+}
+int push(Poz stack, Position P) 
+{
+	Poz Q;
+	Q = malloc(sizeof(Stog));
+	if (Q==NULL)
+	{
+		return PROGRAM_FAIL;
+	}
+	initStog(Q);
+
+	Q->prev = stack;
+	Q->next = stack->next;
+	stack->next = Q;
+	if (Q->next!=NULL)
+		Q->next->prev = Q;
+	Q->adr = P;
+
+	return PROGRAM_SUCCESS;
+}
+Position pop(Poz stack) 
+{
+	Poz temp;
+	Position adr;
+	adr = NULL;temp = NULL;
+
+	temp = stack->next;
+	if (temp->next!=NULL)
+		temp->next->prev = stack;
+	stack->next = temp->next;
+	adr = temp->adr;
+	initStog(temp);
+	free(temp);
+	return adr;
+}
+int deleteTree(Position P) 
+{
+	if (P == NULL)
+		return PROGRAM_SUCCESS;
+	deleteTree(P->child);
+	deleteTree(P->sibling);
+	free(P);
+	return PROGRAM_SUCCESS;
+}
+int emptyStack(Poz stack, Position *root)
+{
+	while (stack->next != NULL) {
+		*root = pop(stack);
+	}
+	return PROGRAM_SUCCESS;
 }
